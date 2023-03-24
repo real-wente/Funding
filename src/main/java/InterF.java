bf2d69a5ac8ab53802cedca13004b69d28e80eee
wentao <614935302@qq.com>
2023-03-24 17:52:46 +0800

    final

---
+++ src/main/java/InterF.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+/**
+ * @author wentao
+ * @date 2023/3/24 17:02
+ */
+
+public class InterF {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.toString());
+
+    }
+}
